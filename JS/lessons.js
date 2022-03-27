70c67d4734579b020edcfad81de684951fe53d06
Oleg <mr.lost@rambler.ru>
2022-03-27 19:15:51 +0300

    wrote code from lesson number 24

--- JS/lessons.js
+++ JS/lessons.js
@@ -39,7 +39,83 @@
 
 // lesson 21 Массивы и псевдомассивы
 
-const arr = [1, 2, 3, 6, 8];
+// const arr = [1, 2, 3, 6, 8];
 
-arr.pop();
-console.log(arr); 
+// arr.pop();
+// console.log(arr); 
+
+//-------------------------------------------------------------------------------------------------------------------
+
+// lesson 22 Создвние копий массивов, объектов и т.п.
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     } 
+// };
+
+// const newNumbers = copy(numbers); 
+
+// newNumbers.a = 10;
+
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// }; 
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);  
+// // console.log(clone);  
+
+// const oldArry = ['a', 'b', 'c'];
+// const newArry = oldArry.slice();
+
+// newArry[1] = 'ababsbabsba';
+
+// console.log(newArry); 
+// console.log(oldArry); 
+
+// // Оператор разворота 
+
+// const video = ['YOUTUBE', 'VIMEO', 'RUTUBE'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];
+
+// console.log(internet); 
+
+
+// //-------------------------------------------------------------------------------------------------------------------
+
+// // lesson 23 Основы ООП, прототипоно-ориентрированное наследование
+
+// const solider = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
+// };
+
+// const jonh = Object.create(solider );
+// jonh.sayHello();
+
+// Object.setPrototypeOf(jonh, solider);
--- JS/script.js
+++ JS/script.js
@@ -1,72 +1,77 @@
 'use strict';
 
-let numberOfFilms;
-
-
-
-function start () {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || 
-          numberOfFilms == null ||
-          isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-// start();
-
 const personalMoviDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function rememberMyFilms () {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-                b = prompt('На сколько вы оцените его?', '');
-
-        if (a != null && b != null && a != '' && b != '' & a.length < 50) {
-            personalMoviDB.movies[a] = b; 
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
+    privat: false,
+    start: function() {
+        personalMoviDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMoviDB.count == '' || 
+        personalMoviDB.count == null ||
+              isNaN(personalMoviDB.count)) {
+                personalMoviDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
         }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                    b = prompt('На сколько вы оцените его?', '');
+    
+            if (a != null && b != null && a != '' && b != '' & a.length < 50) {
+                personalMoviDB.movies[a] = b; 
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMoviDB.count < 10) {
+            console.log('Просмотренно довольно мало фильмов');
+        } else if (personalMoviDB.count >= 10 && personalMoviDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMoviDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+             console.log(personalMoviDB);
+        } 
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMoviDB.privat == false) {
+            personalMoviDB.privat = true;
+        } else if (personalMoviDB.privat == true) {
+            personalMoviDB.privat = false;
+        }
+    },
+    writeYourGeneres: function() {
+        for (let i = 1; i <= 3; i++ ) {
+            let generNum = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            if (generNum == '' || generNum == null ) {
+                i--;
+            } else { 
+                personalMoviDB.genres[i - 1] = generNum;
+            }
+        }
+        personalMoviDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
-}
+    
+};
+personalMoviDB.toggleVisibleMyDB();
+personalMoviDB.toggleVisibleMyDB();
+personalMoviDB.start();
+personalMoviDB.rememberMyFilms();
+personalMoviDB.detectPersonalLevel();
+personalMoviDB.showMyDB(personalMoviDB.privat);
+personalMoviDB.writeYourGeneres();
 
-// rememberMyFilms();
-
-function detectPersonalLevel () {
-    if (personalMoviDB.count < 10) {
-        console.log('Просмотренно довольно мало фильмов');
-    } else if (personalMoviDB.count >= 10 && personalMoviDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMoviDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-
-// detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-         console.log(personalMoviDB);
-    } 
-}
-// showMyDB(personalMoviDB.privat);
-
-function writeYourGeneres () {
-    for (let i = 1; i <= 3; i++ ) {
-        personalMoviDB.genres[i - 1] = 
-        prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-// writeYourGeneres();
